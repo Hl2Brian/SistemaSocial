d986627a95995009004246c9c5238c6fd35f07f8
Brian Fuenzalida <brianfc87@gmail.com>
2025-07-03 22:46:09 -0400

    Modificaciones

--- SistemaSocial/Controllers/InformeSocialController.cs
+++ SistemaSocial/Controllers/InformeSocialController.cs
@@ -12,7 +12,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
-
+ 
 namespace SistemaSocial.Controllers
 {
     [Authorize(Policy = "PolicyAdmin")]
